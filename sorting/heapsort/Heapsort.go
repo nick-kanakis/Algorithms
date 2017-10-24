341dc041dad0d00f3b93fe8a040f02881fee242f
nicolasmanic <nicolas.kanakis@gmail.com>
2017-10-24 19:27:05 +0300

    Heap Sort implementation

---
+++ sorting/heapsort/Heapsort.go
@@ -0,0 +1,57 @@
+package heapsort
+
+func Sort(unsortedArray []int )  {
+
+	//Create a Max Heap
+	buildMaxHeap(unsortedArray)
+	//Swap and remove root element
+	originalSize := len(unsortedArray);
+
+	for i:=0; i < originalSize; i++{
+		lastChildPosition := len(unsortedArray) - 1
+		unsortedArray[0], unsortedArray[lastChildPosition] = unsortedArray[lastChildPosition], unsortedArray[0]
+		maxHeapify(unsortedArray[:lastChildPosition], 0)
+		unsortedArray = unsortedArray[:lastChildPosition]
+	}
+}
+
+
+func buildMaxHeap(unsortedArray []int) {
+	for i := len(unsortedArray)/2; i >= 0; i-- {
+		maxHeapify(unsortedArray, i)
+	}
+}
+
+func maxHeapify(unsortedArray []int, i int) {
+	maxChildPosition := returnMaxChildPosition(unsortedArray,i)
+
+	if maxChildPosition != -1 && unsortedArray[i] < unsortedArray[maxChildPosition]{
+		unsortedArray[i], unsortedArray[maxChildPosition] = unsortedArray[maxChildPosition], unsortedArray[i]
+		maxHeapify(unsortedArray, maxChildPosition)
+	}
+}
+
+func returnMaxChildPosition(array []int, parentPosition int) int{
+
+	length := len(array)
+	leftChildPosition := parentPosition*2 + 1
+	rightChildPosition := leftChildPosition + 1
+
+	//No children
+	if leftChildPosition >= length {
+		return -1
+	}
+
+	//Only left children
+	if rightChildPosition >= length {
+		return leftChildPosition
+	}
+
+	//2 Children
+	if array[leftChildPosition] > array[rightChildPosition] {
+		return leftChildPosition
+	} else {
+		return rightChildPosition
+	}
+
+}
---
+++ sorting/heapsort/Heapsort_test.go
@@ -0,0 +1,69 @@
+package heapsort
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestPrivateReturnMaxChildPosition(t *testing.T) {
+
+	unsortedArray := []int{10, 4, 2, 8, 15, 23}
+	returnedValue := returnMaxChildPosition(unsortedArray, 0)
+	if 1 != returnedValue {
+		fmt.Println("Position: ", returnedValue)
+		t.Error()
+	}
+
+	returnedValue = returnMaxChildPosition(unsortedArray, 1)
+	if 4 != returnedValue {
+		fmt.Println("Position: ", returnedValue)
+		t.Error()
+	}
+
+	returnedValue = returnMaxChildPosition(unsortedArray, 2)
+	if 5 != returnedValue {
+		fmt.Println("Position: ", returnedValue)
+		t.Error()
+	}
+
+	returnedValue = returnMaxChildPosition(unsortedArray, 3)
+	if -1 != returnedValue {
+		fmt.Println("Position: ", returnedValue)
+		t.Error()
+	}
+
+	returnedValue = returnMaxChildPosition(unsortedArray, 4)
+	if -1 != returnedValue {
+		fmt.Println("Position: ", returnedValue)
+		t.Error()
+	}
+}
+
+func TestSort(t *testing.T) {
+
+	heap := []int{4, 10, 3, 5, 1}
+	fmt.Println(heap)
+	Sort(heap)
+	fmt.Println(heap)
+
+	for i := 0; i < len(heap)-2; i++ {
+		if heap[i] > heap[i+1] {
+			t.Error()
+		}
+	}
+}
+
+func TestPrivateBuildMaxHeap(t *testing.T) {
+
+	heap := []int{4, 10, 3, 5, 1}
+	fmt.Println(heap)
+	buildMaxHeap(heap)
+	fmt.Println(heap)
+
+	for i := len(heap) - 1; i >= 0; i-- {
+		parentPosition := (i - 1) / 2
+		if (heap[i] > heap[parentPosition]) {
+			t.Error()
+		}
+	}
+}
