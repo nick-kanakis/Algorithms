b64f7f903b33c41bcc134ede26ea71a734bff773
nicolasmanic <nicolas.kanakis@gmail.com>
2017-10-24 20:44:51 +0300

    Add explanation

--- sorting/heapsort/Heapsort.go
+++ sorting/heapsort/Heapsort.go
@@ -4,22 +4,33 @@ func Sort(unsortedArray []int) {
 
 	//Create a Max Heap
 	buildMaxHeap(unsortedArray)
-	//Swap and remove root element
+
 	originalSize := len(unsortedArray);
+
+	//Swap root element with last child of unsorted port
+	//and re-heapify to fix the problem caused by the swap.
 	for i := 0; i < originalSize; i++ {
 		lastChildPosition := len(unsortedArray) - 1
 		unsortedArray[0], unsortedArray[lastChildPosition] = unsortedArray[lastChildPosition], unsortedArray[0]
 		maxHeapify(unsortedArray[:lastChildPosition], 0)
+
+		// the slice of [start: len(originalArray -1] is the new heap that we need for our next iteration.
 		unsortedArray = unsortedArray[:lastChildPosition]
 	}
 }
 
+// For  size(OriginalArray)/2 to 0
+// Call heapify, that will make the sub-tree under i a heap.
 func buildMaxHeap(unsortedArray []int) {
 	for i := len(unsortedArray) / 2; i >= 0; i-- {
 		maxHeapify(unsortedArray, i)
 	}
 }
 
+// 		Find the maxChild Of of the currentParent
+//		If you need to swap elements
+//			swap and call again the method for the maxChild in order to fix
+//			any problems from the previous step.
 func maxHeapify(unsortedArray []int, i int) {
 	maxChildPosition := returnMaxChildPosition(unsortedArray, i)
 
@@ -35,7 +46,7 @@ func returnMaxChildPosition(array []int, parentPosition int) int {
 	leftChildPosition := parentPosition*2 + 1
 	rightChildPosition := leftChildPosition + 1
 
-	//No children
+	//No children, if there is no left child it can not have a right one.
 	if leftChildPosition >= length {
 		return -1
 	}
